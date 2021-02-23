82e299d2c9f12af7253d8b3fbcb4a0c7ea85d11b
Gonzalo De Spirito <gonzunigad@users.noreply.github.com>
2021-02-23 21:37:53 +0000

    Apply fixes from StyleCI

--- webpay/libwebpay/CreatePdf.php
+++ webpay/libwebpay/CreatePdf.php
@@ -1,27 +1,30 @@
 <?php
-require_once('../../../config/config.inc.php');
-if (!defined('_PS_VERSION_')) exit;
 
-require_once(_PS_MODULE_DIR_.'webpay/vendor/autoload.php');
-require_once('ReportPdfLog.php');
-require_once('HealthCheck.php');
+require_once '../../../config/config.inc.php';
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
 
-$config = array(
-    "ENVIRONMENT" => Configuration::get('WEBPAY_ENVIRONMENT'),
-    "API_KEY" => Configuration::get('WEBPAY_API_KEY_SECRET'),
-    "COMMERCE_CODE" => Configuration::get('WEBPAY_STOREID'),
-    'ECOMMERCE' => 'prestashop'
-);
+require_once _PS_MODULE_DIR_.'webpay/vendor/autoload.php';
+require_once 'ReportPdfLog.php';
+require_once 'HealthCheck.php';
 
-$document = $_POST["document"];
+$config = [
+    'ENVIRONMENT'   => Configuration::get('WEBPAY_ENVIRONMENT'),
+    'API_KEY'       => Configuration::get('WEBPAY_API_KEY_SECRET'),
+    'COMMERCE_CODE' => Configuration::get('WEBPAY_STOREID'),
+    'ECOMMERCE'     => 'prestashop',
+];
+
+$document = $_POST['document'];
 $healthcheck = new HealthCheck($config);
 $json = $healthcheck->printFullResume();
 
 $temp = json_decode($json);
-if ($document == "report"){
+if ($document == 'report') {
     unset($temp->php_info);
 } else {
-    $temp = array('php_info' => $temp->php_info);
+    $temp = ['php_info' => $temp->php_info];
 }
 
 $rl = new ReportPdfLog($document);
