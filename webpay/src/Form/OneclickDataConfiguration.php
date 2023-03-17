303a5c20b4816307bdd26a3c83575d537d3f6388
Miguel Vargas <mvarlic@gmail.com>
2023-03-17 17:13:08 -0500

    add checkbox to activate

--- webpay/src/Form/OneclickDataConfiguration.php
+++ webpay/src/Form/OneclickDataConfiguration.php
@@ -14,6 +14,7 @@ final class OneclickDataConfiguration implements DataConfigurationInterface
     public const ONECLICK_API_KEY = 'ONECLICK_API_KEY';
     public const ONECLICK_ENVIRONMENT = 'ONECLICK_ENVIRONMENT';
     public const ONECLICK_DEFAULT_ORDER_STATE_ID_AFTER_PAYMENT = 'ONECLICK_DEFAULT_ORDER_STATE_ID_AFTER_PAYMENT';
+    public const ONECLICK_ACTIVE = 'ONECLICK_ACTIVE';
 
     /**
      * @var ConfigurationInterface
@@ -33,12 +34,14 @@ final class OneclickDataConfiguration implements DataConfigurationInterface
      */
     public function getConfiguration(): array
     {
+        $oneclickActive = $this->configuration->get(static::ONECLICK_ACTIVE);
         $oneclickEnviroment = $this->configuration->get(static::ONECLICK_ENVIRONMENT);
         $oneclickMallCommerceCode = $this->configuration->get(static::ONECLICK_MALL_COMMERCE_CODE);
         $oneclickChildCommerceCode = $this->configuration->get(static::ONECLICK_CHILD_COMMERCE_CODE);
         $oneclickApikey = $this->configuration->get(static::ONECLICK_API_KEY);
         $oneclickDefaultOrderStateIdAfterPayment = $this->configuration->get(static::ONECLICK_DEFAULT_ORDER_STATE_ID_AFTER_PAYMENT);
         return [
+            'form_oneclick_active' => $oneclickActive,
             'form_oneclick_environment' => $oneclickEnviroment,
             'form_oneclick_mall_commerce_code' => $oneclickMallCommerceCode,
             'form_oneclick_child_commerce_code' => $oneclickChildCommerceCode,
@@ -52,6 +55,7 @@ final class OneclickDataConfiguration implements DataConfigurationInterface
      */
     public function updateConfiguration(array $configuration): array
     {
+        $this->configuration->set(static::ONECLICK_ACTIVE, $configuration['form_oneclick_active']);
         $this->configuration->set(static::ONECLICK_MALL_COMMERCE_CODE, $configuration['form_oneclick_mall_commerce_code']);
         $this->configuration->set(static::ONECLICK_CHILD_COMMERCE_CODE, $configuration['form_oneclick_child_commerce_code']);
         $this->configuration->set(static::ONECLICK_API_KEY, $configuration['form_oneclick_api_key']);
