033d50cfb06c7b5e17dafe275b54fdf91456b249
Miguel Vargas <mvarlic@gmail.com>
2023-09-25 22:43:43 -0500

    feat: update tabs trait

--- webpay/src/Helpers/InteractsWithTabs.php
+++ webpay/src/Helpers/InteractsWithTabs.php
@@ -4,22 +4,18 @@ namespace PrestaShop\Module\WebpayPlus\Helpers;
 
 use PrestaShop\Module\WebpayPlus\Helpers\TabsHelper;
 use PrestaShop\Module\WebpayPlus\Controller\Admin\ConfigureController;
-use PrestaShop\Module\WebpayPlus\Controller\Admin\OrdersController;
-use PrestaShop\Module\WebpayPlus\Utils\Utils;
+use PrestaShop\Module\WebpayPlus\Helpers\PrestashopUtil;
 use Language;
-/**
- * Trait InteractsWithTabs.
- */
+
 trait InteractsWithTabs
 {
     protected function installTab(){
         
-        if (Utils::isPrestashopEqualOrGreater_1_7_1()) {
+        if (PrestashopUtil::isPrestashopEqualOrGreater_1_7_1()) {
             return;
         }
         TabsHelper::removeTab('WebPay');
-        TabsHelper::AddTab(ConfigureController::TAB_CLASS_NAME, $this->getNamesToManualInstall('Configuración Webpay', 'Modules.WebpayPlus.Config'), 'WebPay', 'AdminParentPayment');
-        //TabsHelper::AddTab(OrdersController::TAB_CLASS_NAME, $this->getNamesToManualInstall('Órdenes Webpay', 'Modules.WebpayPlus.Orders'), 'WebPay', 'AdminParentOrders');
+        TabsHelper::addTab(ConfigureController::TAB_CLASS_NAME, $this->getNamesToManualInstall('Configuración Webpay', 'Modules.WebpayPlus.Config'), 'WebPay', 'AdminParentPayment');
     }
     
     protected function uninstallTab()
@@ -28,25 +24,18 @@ trait InteractsWithTabs
     }
 
     protected function addTabs($base){
-        if (!Utils::isPrestashopEqualOrGreater_1_7_1()) {
+        if (!PrestashopUtil::isPrestashopEqualOrGreater_1_7_1()) {
             return;
         }
 
         $base->tabs = [
-            [   /*'icon' => 'school',*/
-                'route_name' => 'ps_controller_webpay_configure',
+            [
+                'route_name' => 'ps_controller_configure',
                 'class_name' => ConfigureController::TAB_CLASS_NAME,
                 'visible' => true,
                 'name' => $this->getNames('Configuración Webpay', 'Modules.WebpayPlus.Config'),
                 'parent_class_name' => 'AdminParentPayment',
-            ],/*
-            [
-                'route_name' => 'ps_controller_webpay_orders',
-                'class_name' => OrdersController::TAB_CLASS_NAME,
-                'visible' => true,
-                'name' => $this->getNames('Órdenes Webpay', 'Modules.WebpayPlus.Orders'),
-                'parent_class_name' => 'AdminParentOrders',
-            ],*/
+            ]
         ];
     }
 
