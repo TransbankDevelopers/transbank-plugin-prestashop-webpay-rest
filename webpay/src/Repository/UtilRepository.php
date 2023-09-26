41fb1424cec251103bb886c2ddfe06ded05febae
Miguel Vargas <mvarlic@gmail.com>
2023-09-25 22:27:29 -0500

    feat: add util repository

---
+++ webpay/src/Repository/UtilRepository.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace PrestaShop\Module\WebpayPlus\Repository;
+
+use Transbank\Plugin\Repository\IUtilRepository;
+use Db;
+use Exception;
+
+class UtilRepository implements IUtilRepository {
+    
+    public function executeSql($sql){
+        return Db::getInstance()->executeS($sql);
+    }
+
+    public function getRow($sql){
+        return Db::getInstance()->getRow($sql);
+    }
+
+    public function getValue($sql){
+        return Db::getInstance()->getValue($sql, $use_cache = true);
+    }
+
+    public function sanitizeValue($value){
+        return pSQL($value);
+    }
+
+}
