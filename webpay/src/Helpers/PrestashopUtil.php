f4c6435947ee3e5ea5b956e14147edfd5905823e
Miguel Vargas <mvarlic@gmail.com>
2023-09-25 22:26:29 -0500

    feat: add prestashop util

---
+++ webpay/src/Helpers/PrestashopUtil.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace PrestaShop\Module\WebpayPlus\Helpers;
+
+class PrestashopUtil
+{
+    public static function getFullVersionPrestashop()
+    {
+        return _PS_VERSION_;
+    }
+
+    public static function getBaseVersionPrestashop()
+    {
+        return substr(_PS_VERSION_, 0, 3);
+    }
+
+    public static function isPrestashop_1_7()
+    {
+        return self::getBaseVersionPrestashop() == '1.7' ? true : false;
+    }
+
+    public static function isPrestashopEqualOrGreater_1_7_1()
+    {
+        return version_compare(_PS_VERSION_, '1.7.1', '>=');
+    }
+}
