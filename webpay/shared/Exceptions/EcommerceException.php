a4fdbeb3f5929405a068f226048858b76d020019
Miguel Vargas <mvarlic@gmail.com>
2023-10-18 14:33:31 -0500

    refactor: replace deault code with constant

--- webpay/shared/Exceptions/EcommerceException.php
+++ webpay/shared/Exceptions/EcommerceException.php
@@ -2,9 +2,11 @@
 
 namespace Transbank\Plugin\Exceptions;
 
+use Transbank\Plugin\Helpers\ExceptionConstans;
+
 class EcommerceException extends \Exception
 {
     public function __construct($message, \Exception $previous = null) {
-        parent::__construct($message, 0, $previous);
+        parent::__construct($message, ExceptionConstans::DEFAULT_CODE, $previous);
     }
 }
