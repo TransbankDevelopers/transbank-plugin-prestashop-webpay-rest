b387317526d8ee2b04de38d52c14af3e97df2cd3
Miguel Vargas <mvarlic@gmail.com>
2023-10-19 08:59:01 -0500

    fix: replace ran for random_int

--- webpay/src/Controller/BaseModuleFrontController.php
+++ webpay/src/Controller/BaseModuleFrontController.php
@@ -127,7 +127,7 @@ class BaseModuleFrontController extends ModuleFrontController
         $characters = '0123456789abcdefghijklmnopqrstuvwxyz';
         $randomString = '';
         for ($i = 0; $i < $length; $i++) {
-            $randomString .= $characters[rand(0, strlen($characters) - 1)];
+            $randomString .= $characters[random_int(0, strlen($characters) - 1)];
         }
         return $randomString;
     }
