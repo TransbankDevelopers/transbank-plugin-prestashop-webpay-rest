04a3d76b7906367316eb0e814ed101937291d485
Mauricio Astudillo Toledo <m.astudillo.toledo@gmail.com>
2024-04-30 16:51:15 -0400

    refactor: remove unnecessary files

--- webpay/js/transactions/index.js
+++
@@ -1,9 +0,0 @@
-import Grid from "@PSJs/components/grid/grid";
-import FiltersResetExtension from "@PSJs/components/grid/extension/filters-reset-extension";
-
-const { $ } = window;
-
-$(() => {
-  const grid = new Grid("transactions");
-  grid.addExtension(new FiltersResetExtension());
-});
