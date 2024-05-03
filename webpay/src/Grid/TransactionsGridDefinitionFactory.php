8fbced7662b3b5ba5895fd7be9120eba98d11bb4
Mauricio Astudillo Toledo <m.astudillo.toledo@gmail.com>
2024-05-03 18:29:06 -0400

    refactor: reduce complexity

--- webpay/src/Grid/TransactionsGridDefinitionFactory.php
+++ webpay/src/Grid/TransactionsGridDefinitionFactory.php
@@ -4,7 +4,7 @@ namespace PrestaShop\Module\WebpayPlus\Grid;
 
 use PrestaShop\PrestaShop\Core\Grid\Definition\Factory\AbstractFilterableGridDefinitionFactory;
 use PrestaShop\PrestaShop\Core\Grid\Column\ColumnCollection;
-use PrestaShop\PrestaShop\Core\Grid\Column\Type\DataColumn;
+use PrestaShop\PrestaShop\Core\Grid\Column\Type\Common\DataColumn;
 use PrestaShop\PrestaShop\Core\Grid\Column\Type\Common\DateTimeColumn;
 use PrestaShop\PrestaShop\Core\Grid\Filter\Filter;
 use PrestaShop\PrestaShop\Core\Grid\Filter\FilterCollection;
@@ -15,7 +15,7 @@ use PrestaShopBundle\Form\Admin\Type\DateRangeType;
 use PrestaShopBundle\Form\Admin\Type\SearchAndResetType;
 use PrestaShop\PrestaShop\Core\Grid\Column\Type\Common\ActionColumn;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use PrestaShop\PrestaShop\Core\Grid\Column\Type\ColorColumn;
+use PrestaShop\PrestaShop\Core\Grid\Column\Type\Common\ColorColumn;
 use PrestaShop\Module\WebpayPlus\Grid\TransactionsStatusChoiceProvider;
 use PrestaShop\PrestaShop\Core\Form\FormChoiceProviderInterface;
 
@@ -40,161 +40,122 @@ final class TransactionsGridDefinitionFactory extends AbstractFilterableGridDefi
 
     protected function getColumns()
     {
-        return (new ColumnCollection())
-            ->add((new DataColumn('cart_id'))
-                    ->setName('Id del carrito')
-                    ->setOptions([
-                        'field' => 'cart_id',
-                    ])
-            )
-            ->add((new DataColumn('order_id'))
-                    ->setName('Id del pedido')
-                    ->setOptions([
-                        'field' => 'order_id',
-                    ])
-            )
-            ->add((new DataColumn('response_code'))
-                    ->setName('Código respuesta')
-                    ->setOptions([
-                        'field' => 'response_code',
-                    ])
-            )
-            ->add((new DataColumn('vci'))
-                    ->setName('VCI')
-                    ->setOptions([
-                        'field' => 'vci',
-                    ])
-            )
-            ->add((new DataColumn('amount'))
-                    ->setName('Monto')
-                    ->setOptions([
-                        'field' => 'amount',
-                    ])
-            )
-            ->add((new DataColumn('iso_code'))
-                    ->setName('Moneda')
-                    ->setOptions([
-                        'field' => 'iso_code',
-                    ])
-            )->add((new DataColumn('card_number'))
-                    ->setName('Nº. Tarjeta.')
-                    ->setOptions([
-                        'field' => 'card_number',
-                    ])
-            )->add((new DataColumn('token'))
-                    ->setName('Token')
-                    ->setOptions([
-                        'field' => 'token',
-                    ])
-            )
-            ->add((new ColorColumn('status'))
-                ->setName($this->trans('Status', [], 'Admin.Global'))
-                ->setOptions([
-                    'field' => 'status',
-                    'color_field' => 'status_color',
-                ]))
-            ->add((new DataColumn('environment'))
-                ->setName('Ambiente')
-                ->setOptions(['field' => 'environment']))
-            ->add((new DataColumn('product'))
-                ->setName('Producto')
-                ->setOptions(['field' => 'product']))
-            ->add((new DateTimeColumn('created_at'))
-                    ->setName('Fecha de Trans.')
-                    ->setOptions([
-                        'field' => 'created_at',
-                    ])
-            )->add((new ActionColumn('actions'))
-                ->setName('Acciones'));
+        $gridColumns = $this->getGridColumnsData();
+        $columnCollection = new ColumnCollection();
+
+        foreach ($gridColumns as $key => $columnName) {
+            if ($key === 'actions') {
+                $columnCollection->add((new ActionColumn($key))
+                        ->setName($columnName)
+                );
+                continue;
+            }
+
+            if ($key === 'status') {
+                $columnCollection->add((new ColorColumn($key))
+                        ->setName($columnName)
+                        ->setOptions([
+                            'field' => $key,
+                            'color_field' => 'status_color',
+                        ])
+                );
+                continue;
+            }
+
+            if ($key === 'created_at') {
+                $columnCollection->add((new DateTimeColumn($key))
+                        ->setName($columnName)
+                        ->setOptions(['field' => $key])
+                );
+                continue;
+            }
+
+            $columnCollection->add((new DataColumn($key))
+                    ->setName($columnName)
+                    ->setOptions(['field' => $key])
+            );
+        }
+
+        return $columnCollection;
     }
 
     protected function getFilters(): FilterCollectionInterface
     {
-        return (new FilterCollection())->add(
-            (new Filter('cart_id', TextType::class))
-                ->setAssociatedColumn('cart_id')
-                ->setTypeOptions([
-                    'required' => false
-                ])
-        )->add(
-            (new Filter('order_id', TextType::class))
-                ->setAssociatedColumn('order_id')
-                ->setTypeOptions([
-                    'required' => false
-                ])
-        )->add(
-            (new Filter('response_code', TextType::class))
-                ->setAssociatedColumn('response_code')
-                ->setTypeOptions([
-                    'required' => false
-                ])
-        )->add(
-            (new Filter('vci', TextType::class))
-                ->setAssociatedColumn('vci')
-                ->setTypeOptions([
-                    'required' => false
-                ])
-        )->add(
-            (new Filter('amount', NumberType::class))
-                ->setAssociatedColumn('amount')
-                ->setTypeOptions([
-                    'required' => false
-                ])
-        )
-            ->add(
-                (new Filter('iso_code', TextType::class))
-                    ->setAssociatedColumn('iso_code')
-                    ->setTypeOptions([
-                        'required' => false
-                    ])
-            )
-            ->add(
-                (new Filter('card_number', TextType::class))
-                    ->setAssociatedColumn('card_number')
-                    ->setTypeOptions([
-                        'required' => false
-                    ])
-            )->add(
-                (new Filter('token', TextType::class))
-                    ->setAssociatedColumn('token')
-                    ->setTypeOptions([
-                        'required' => false
-                    ])
-            )->add(
-                (new Filter('status', ChoiceType::class))
-                    ->setAssociatedColumn('status')
-                    ->setTypeOptions([
-                        'choices' => (new TransactionsStatusChoiceProvider())->getChoices(),
-                        'required' => false
-                    ])
-            )->add(
-                (new Filter('product', TextType::class))
-                    ->setAssociatedColumn('product')
-                    ->setTypeOptions([
-                        'required' => false
-                    ])
-            )->add(
-                (new Filter('environment', TextType::class))
-                    ->setAssociatedColumn('environment')
-                    ->setTypeOptions([
-                        'required' => false
-                    ])
-            )->add(
-                (new Filter('created_at', DateRangeType::class))
-                    ->setAssociatedColumn('created_at')
-                    ->setTypeOptions([
-                        'required' => false
-                    ])
-            )->add(
-                (new Filter('actions', SearchAndResetType::class))
-                    ->setAssociatedColumn('actions')
-                    ->setTypeOptions([
-                        'reset_route' => 'admin_common_reset_search_by_filter_id',
-                        'reset_route_params' => [
-                            'filterId' => self::GRID_ID,
-                        ],
-                        'redirect_route' => 'ps_controller_webpay_transaction_list',
-                    ])
+        $gridColumns = $this->getGridColumnsData();
+        $filterCollection = new FilterCollection();
+
+        foreach ($gridColumns as $key => $columnName) {
+            if ($key === 'amount') {
+                $filterCollection->add(
+                    (new Filter($key, NumberType::class))
+                        ->setAssociatedColumn($key)
+                        ->setTypeOptions(['required' => false])
+                );
+                continue;
+            }
+
+            if ($key === 'status') {
+                $filterCollection->add(
+                    (new Filter($key, ChoiceType::class))
+                        ->setAssociatedColumn($key)
+                        ->setTypeOptions([
+                            'choices' => (new TransactionsStatusChoiceProvider())->getChoices(),
+                            'required' => false
+                        ])
+                );
+                continue;
+            }
+
+            if ($key === 'created_at') {
+                $filterCollection->add(
+                    (new Filter($key, DateRangeType::class))
+                        ->setAssociatedColumn($key)
+                        ->setTypeOptions(['required' => false])
+                );
+                continue;
+            }
+
+            if ($key === 'actions') {
+                $filterCollection->add(
+                    (new Filter($key, SearchAndResetType::class))
+                        ->setAssociatedColumn($key)
+                        ->setTypeOptions([
+                            'reset_route' => 'admin_common_reset_search_by_filter_id',
+                            'reset_route_params' => [
+                                'filterId' => self::GRID_ID,
+                            ],
+                            'redirect_route' => 'ps_controller_webpay_transaction_list',
+                        ])
+                );
+                continue;
+            }
+
+            $filterCollection->add(
+                (new Filter($key, TextType::class))
+                    ->setAssociatedColumn($key)
+                    ->setTypeOptions(['required' => false])
             );
+        }
+
+        return $filterCollection;
+    }
+
+    private function getGridColumnsData(): array
+    {
+        return [
+            'cart_id' => 'Id del carrito',
+            'order_id' => 'Id del pedido',
+            'response_code' => 'Código de respuesta',
+            'vci' => 'VCI',
+            'amount' => 'Monto',
+            'iso_code' => 'Moneda',
+            'card_number' => '4 últimos dígitos tarjeta',
+            'token' => 'Token',
+            'status' => 'Estado',
+            'environment' => 'Ambiente',
+            'product' => 'Producto',
+            'created_at' => 'Fecha transacción',
+            'actions' => 'Acciones',
+        ];
     }
 }
