320f363012a1fc131f7b34fb3b0bf200a6c9ec80
Mauricio Astudillo Toledo <m.astudillo.toledo@gmail.com>
2024-05-06 16:57:19 -0400

    refactor: add reference to Prestashop doc

--- webpay/views/js/grid.transactions.js
+++ webpay/views/js/grid.transactions.js
@@ -1,3 +1,5 @@
+// Reference: https://devdocs.prestashop-project.org/1.7/development/components/global-components/#how-to-use
+
 $(document).ready(function () {
     const $ = window.$;
     const Grid = window.prestashop.component.Grid;
