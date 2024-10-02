e2b89cb459b8c1607c0ec4369dc57324032a9360
Mauricio Astudillo Toledo <m.astudillo.toledo@gmail.com>
2024-10-02 18:08:56 -0300

    refactor: move custom link columns to grid folder

--- webpay/src/Grid/TransactionsGridDefinitionFactory.php
+++ webpay/src/Grid/TransactionsGridDefinitionFactory.php
@@ -2,7 +2,7 @@
 
 namespace PrestaShop\Module\WebpayPlus\Grid;
 
-use PrestaShop\Module\WebpayPlus\Core\Grid\Column\Type\CustomLinkColumn;
+use PrestaShop\Module\WebpayPlus\Grid\Column\Type\CustomLinkColumn;
 use PrestaShop\PrestaShop\Core\Grid\Definition\Factory\AbstractFilterableGridDefinitionFactory;
 use PrestaShop\PrestaShop\Core\Grid\Column\ColumnCollection;
 use PrestaShop\PrestaShop\Core\Grid\Column\Type\Common\DataColumn;
