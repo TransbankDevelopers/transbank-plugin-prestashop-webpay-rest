fb81f1ae4b870ecbfdf3c3ffcdf075c6d1b12b58
Mauricio Astudillo Toledo <m.astudillo.toledo@gmail.com>
2024-12-30 18:39:00 -0300

    style: format file BaseModuleFrontController

--- webpay/src/Controller/BaseModuleFrontController.php
+++ webpay/src/Controller/BaseModuleFrontController.php
@@ -17,68 +17,80 @@ class BaseModuleFrontController extends ModuleFrontController
     public $ssl = true;
     protected $logger = null;
 
-    protected function getCustomer($customerId){
+    protected function getCustomer($customerId)
+    {
         return new Customer($customerId);
     }
 
-    protected function getCart($cartId){
+    protected function getCart($cartId)
+    {
         return new Cart($cartId);
     }
 
-    protected function getUserId(){
+    protected function getUserId()
+    {
         if ($this->context->customer->isLogged()) {
             return $this->context->customer->id;
         }
         return null;
     }
 
-    protected function getUserEmail(){
+    protected function getUserEmail()
+    {
         if ($this->context->customer->isLogged()) {
             return $this->context->customer->email;
         }
         return null;
     }
 
-    protected function logError($msg){
+    protected function logError($msg)
+    {
         $this->logger->logError($msg);
     }
 
-    protected function logInfo($msg){
+    protected function logInfo($msg)
+    {
         $this->logger->logInfo($msg);
     }
 
-    protected function cartToLog($cart){
+    protected function cartToLog($cart)
+    {
         $this->logInfo('-----------------------------------------------------');
         $this->logInfo('------------CART-------------------------------------');
         $this->logInfo(json_encode($cart));
         $prods = $cart->getProducts();
-        foreach ($prods as $prod){
+        foreach ($prods as $prod) {
             $this->logInfo('------------PRODUCT---------------------------------');
             $this->logInfo(json_encode($prod));
         }
         $this->logInfo('-----------------------------------------------------');
     }
 
-    protected function customerToLog($customer){
+    protected function customerToLog($customer)
+    {
         $this->logInfo('-----------------------------------------------------');
         $this->logInfo('------------CUSTOMER-------------------------------------');
         $this->logInfo(json_encode($customer));
         $this->logInfo('-----------------------------------------------------');
     }
 
-    protected function getCartFromContext(){
+    protected function getCartFromContext()
+    {
         return Context::getContext()->cart;
     }
 
-    protected function getCustomerFromContext(){
+    protected function getCustomerFromContext()
+    {
         return new Customer($this->getCartFromContext()->id_customer);
     }
 
-    protected function getOrderTotalRound($cart){
+    protected function getOrderTotalRound($cart)
+    {
         return round($this->getOrderTotalOriginal($cart));
     }
 
-    protected function getOrderTotalOriginal($cart){
+    protected function getOrderTotalOriginal($cart)
+    {
         if (!isset($cart))
             $cart = $this->getCartFromContext();
         return $cart->getOrderTotal(true, Cart::BOTH);// for CLP it should alway be a int
@@ -91,10 +103,10 @@ class BaseModuleFrontController extends ModuleFrontController
         $date_tx_fecha = date('d-m-Y');
         $this->logError($errorMessage);
         Context::getContext()->smarty->assign([
-            'WEBPAY_RESULT_CODE'          => 500,
-            'WEBPAY_RESULT_DESC'          => $errorMessage,
-            'WEBPAY_VOUCHER_ORDENCOMPRA'  => 0,
-            'WEBPAY_VOUCHER_TXDATE_HORA'  => $date_tx_hora,
+            'WEBPAY_RESULT_CODE' => 500,
+            'WEBPAY_RESULT_DESC' => $errorMessage,
+            'WEBPAY_VOUCHER_ORDENCOMPRA' => 0,
+            'WEBPAY_VOUCHER_TXDATE_HORA' => $date_tx_hora,
             'WEBPAY_VOUCHER_TXDATE_FECHA' => $date_tx_fecha,
         ]);
         $this->setTemplate('module:webpay/views/templates/front/payment_error.tpl');
@@ -106,20 +118,24 @@ class BaseModuleFrontController extends ModuleFrontController
         Tools::redirect($redirectTo);
     }
 
-    protected function loadCartFromCookie(){
+    protected function loadCartFromCookie()
+    {
         $this->context->cart = new Cart($this->context->cookie->webpay_cart_id);
         return $this->context->cart;
     }
 
-    protected function getUserIdFromCookie(){
+    protected function getUserIdFromCookie()
+    {
         return $this->context->cookie->webpay_customer_id;
     }
 
-    protected function getUserEmailFromCookie(){
+    protected function getUserEmailFromCookie()
+    {
         return $this->context->cookie->webpay_email;
     }
 
-    protected function generateRandomId($length = 10) {
+    protected function generateRandomId($length = 10)
+    {
         $characters = '0123456789abcdefghijklmnopqrstuvwxyz';
         $randomString = '';
         for ($i = 0; $i < $length; $i++) {
@@ -127,6 +143,7 @@ class BaseModuleFrontController extends ModuleFrontController
         }
         return $randomString;
     }
+
 }
 
 
