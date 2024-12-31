0e4a810c30dafe1735107167cb32dfd96e53119c
Mauricio Astudillo Toledo <m.astudillo.toledo@gmail.com>
2024-12-31 17:31:25 -0300

    style: format file

--- webpay/src/Model/TransbankWebpayRestTransaction.php
+++ webpay/src/Model/TransbankWebpayRestTransaction.php
@@ -38,28 +38,28 @@ class TransbankWebpayRestTransaction extends ObjectModel
     public $card_number;
 
     public static $definition = [
-        'table'     => self::TABLE_NAME,
-        'primary'   => 'id',
+        'table' => self::TABLE_NAME,
+        'primary' => 'id',
         'multilang' => false,
-        'fields'    => [
-            'cart_id'            => ['type' => self::TYPE_INT, 'validate' => 'isUnsignedInt', 'required' => true],
-            'buy_order'          => ['type' => self::TYPE_STRING, 'validate' => 'isString', 'required' => true],
-            'order_id'           => ['type' => self::TYPE_STRING, 'validate' => 'isUnsignedInt', 'allow_null' => true],
-            'token'              => ['type' => self::TYPE_STRING, 'validate' => 'isString', 'required' => true],
-            'session_id'         => ['type' => self::TYPE_STRING, 'validate' => 'isString', 'allow_null' => true],
-            'status'             => ['type' => self::TYPE_INT, 'validate' => 'isUnsignedInt', 'required' => true],
-            'response_code'      => ['type' => self::TYPE_INT, 'validate' => 'isInt', 'allow_null' => true],
-            'currency_id'        => ['type' => self::TYPE_INT, 'validate' => 'isUnsignedInt', 'allow_null' => true],
+        'fields' => [
+            'cart_id' => ['type' => self::TYPE_INT, 'validate' => 'isUnsignedInt', 'required' => true],
+            'buy_order' => ['type' => self::TYPE_STRING, 'validate' => 'isString', 'required' => true],
+            'order_id' => ['type' => self::TYPE_STRING, 'validate' => 'isUnsignedInt', 'allow_null' => true],
+            'token' => ['type' => self::TYPE_STRING, 'validate' => 'isString', 'required' => true],
+            'session_id' => ['type' => self::TYPE_STRING, 'validate' => 'isString', 'allow_null' => true],
+            'status' => ['type' => self::TYPE_INT, 'validate' => 'isUnsignedInt', 'required' => true],
+            'response_code' => ['type' => self::TYPE_INT, 'validate' => 'isInt', 'allow_null' => true],
+            'currency_id' => ['type' => self::TYPE_INT, 'validate' => 'isUnsignedInt', 'allow_null' => true],
             'transbank_response' => ['type' => self::TYPE_HTML, 'validate' => 'isString', 'allow_null' => true],
-            'amount'             => ['type' => self::TYPE_FLOAT, 'validate' => 'isPrice', 'required' => true],
-            'vci'                => ['type' => self::TYPE_STRING, 'validate' => 'isString'],
-            'created_at'         => ['type' => self::TYPE_DATE, 'validate' => 'isDate'],
+            'amount' => ['type' => self::TYPE_FLOAT, 'validate' => 'isPrice', 'required' => true],
+            'vci' => ['type' => self::TYPE_STRING, 'validate' => 'isString'],
+            'created_at' => ['type' => self::TYPE_DATE, 'validate' => 'isDate'],
 
-            'environment'               => ['type' => self::TYPE_STRING, 'validate' => 'isString', 'required' => true],
-            'commerce_code'             => ['type' => self::TYPE_STRING, 'validate' => 'isString', 'required' => true],
-            'child_commerce_code'       => ['type' => self::TYPE_STRING, 'validate' => 'isString', 'allow_null' => true],
-            'product'                   => ['type' => self::TYPE_STRING, 'validate' => 'isString', 'required' => true],
-            'card_number'               => ['type' => self::TYPE_STRING, 'validate' => 'isString', 'allow_null' => true],
+            'environment' => ['type' => self::TYPE_STRING, 'validate' => 'isString', 'required' => true],
+            'commerce_code' => ['type' => self::TYPE_STRING, 'validate' => 'isString', 'required' => true],
+            'child_commerce_code' => ['type' => self::TYPE_STRING, 'validate' => 'isString', 'allow_null' => true],
+            'product' => ['type' => self::TYPE_STRING, 'validate' => 'isString', 'required' => true],
+            'card_number' => ['type' => self::TYPE_STRING, 'validate' => 'isString', 'allow_null' => true],
         ],
     ];
 }
