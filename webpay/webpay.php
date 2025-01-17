0fd7af4efd877d8da2b631221c43c2a96bd4cf12
Mauricio Astudillo Toledo <m.astudillo.toledo@gmail.com>
2025-01-17 16:48:13 -0300

    refactor: delete hook name from module hook list

--- webpay/webpay.php
+++ webpay/webpay.php
@@ -24,7 +24,6 @@ class WebPay extends PaymentModule
 
     private const MODULE_HOOKS = [
         'paymentOptions',
-        'paymentReturn',
         'displayBackOfficeHeader',
         'displayHeader',
         'displayPaymentReturn',
