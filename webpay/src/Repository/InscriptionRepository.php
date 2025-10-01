2c9da342b0d140c67d519fea2336e0200fbcf7c3
Mauricio Astudillo Toledo <m.astudillo.toledo@gmail.com>
2025-10-01 08:55:06 -0300

    feat: add deleteInscriptionByUserAndId method to InscriptionRepository

--- webpay/src/Repository/InscriptionRepository.php
+++ webpay/src/Repository/InscriptionRepository.php
@@ -66,4 +66,35 @@ class InscriptionRepository
 
         return !empty($results) ? $results[0] : null;
     }
+
+    /**
+     * Delete an inscription by user ID and inscription ID, only if status is completed.
+     * Delete its limited to one row.
+     *
+     * @param string $userId User ID.
+     * @param string $inscriptionId Inscription ID.
+     *
+     * @return bool True if a row was deleted, false otherwise.
+     */
+    public function deleteInscriptionByUserAndId(string $userId, string $inscriptionId): bool
+    {
+        $tableName = pSQL(_DB_PREFIX_ . TransbankInscriptions::TABLE_NAME);
+        $userId = (int) pSQL($userId);
+        $inscriptionId = (int) pSQL($inscriptionId);
+        $status = TransbankInscriptions::STATUS_COMPLETED;
+
+        if ($userId <= 0 || $inscriptionId <= 0) {
+            return false;
+        }
+
+        $sql = "DELETE FROM {$tableName}
+                WHERE `user_id` = {$userId}
+                  AND `id` = {$inscriptionId}
+                  AND `status` = '{$status}'
+                  LIMIT 1";
+
+        $result = Db::getInstance()->execute($sql);
+
+        return $result && Db::getInstance()->Affected_Rows() > 0;
+    }
 }
