7c7ec5143fdc48f7f34e8caa90bc0aae866fc142
Matias Vasquez <m.vasquezac@gmail.com>
2026-01-07 09:11:10 -0300

    feat: add js for oneclick grid

---
+++ webpay/views/js/grid.cards.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+    const gridId = "oneclick_card_list";
+    const Grid = window.prestashop.component.Grid;
+    const GridExtensions = window.prestashop.component.GridExtensions;
+
+    const cardsGrid = new Grid(gridId);
+    cardsGrid.addExtension(new GridExtensions.FiltersResetExtension());
+    cardsGrid.addExtension(new GridExtensions.SortingExtension());
+    cardsGrid.addExtension(new GridExtensions.SubmitRowActionExtension());
+    cardsGrid.addExtension(new GridExtensions.LinkRowActionExtension());
+
+    if (typeof window.webpayDeleteFailedData !== "undefined") {
+        const deleteData =
+            typeof window.webpayDeleteFailedData === "string"
+                ? JSON.parse(window.webpayDeleteFailedData)
+                : window.webpayDeleteFailedData;
+
+        $("#modalErrorMessage").text(deleteData.error);
+        $("#forceDeleteForm").attr("action", deleteData.forceDeleteUrl);
+        $("#forceDeleteToken").val(deleteData.csrfToken);
+
+        $("#forceDeleteModal").modal("show");
+
+        delete window.webpayDeleteFailedData;
+    }
+});
