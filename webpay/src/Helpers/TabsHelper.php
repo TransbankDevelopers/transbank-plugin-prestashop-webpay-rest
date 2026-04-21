aa6602a4db38045f43ad1e0c81cb01f28ccf2842
Luis Guananga <luis.guananga@continuumhq.com>
2026-04-21 11:07:12 -0500

    fix: refine uninstall and reinitialization flow in PrestaShop 9
    This change refines the module uninstall and reinitialization flow in PrestaShop 9.
    
    It addresses an error that occurred when the Transbank plugin was reinitialized or uninstalled, which could lead to incomplete cleanup or inconsistent module state.

--- webpay/src/Helpers/TabsHelper.php
+++ webpay/src/Helpers/TabsHelper.php
@@ -38,7 +38,7 @@ class TabsHelper
         $container = SymfonyContainer::getInstance();
 
         if ($container !== null) {
-            $tabRepository = $container->get(TabRepository::class);
+            $tabRepository = $container->get('webpay.repository.tab_repository');
             return (int) $tabRepository->findOneIdByClassName($className);
         }
 
