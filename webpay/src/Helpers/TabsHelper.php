4264fea95c69104286cfc57a95e576e084d631cf
Luis Guananga <luis.guananga@continuumhq.com>
2026-04-21 14:41:14 -0500

    refactor: apply suggested changes from Perrobot PR review

--- webpay/src/Helpers/TabsHelper.php
+++ webpay/src/Helpers/TabsHelper.php
@@ -37,7 +37,7 @@ class TabsHelper
     {
         $container = SymfonyContainer::getInstance();
 
-        if ($container !== null) {
+        if ($container !== null && $container->has('webpay.repository.tab_repository')) {
             $tabRepository = $container->get('webpay.repository.tab_repository');
             return (int) $tabRepository->findOneIdByClassName($className);
         }
